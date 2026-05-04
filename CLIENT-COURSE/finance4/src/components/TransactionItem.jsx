54ac9bad5d7d317834c9ec5b39282eebf86040e9
Rafa_Khadafi <rafa.engineer.dev@gmail.com>
2026-05-04 14:10:22 +0700

    latihan 4finance

--- CLIENT-COURSE/finance4/src/components/TransactionItem.jsx
+++ CLIENT-COURSE/finance4/src/components/TransactionItem.jsx
@@ -1,23 +1,47 @@
+import { transactionApi } from "../api/transaction";
+import { walletApi } from "../api/wallet";
+
 export default TransactionItem = ({showDate, transaction, onDelete}) => {
-    return <>            <div className="cursor-pointer flex lg:items-center justify-between border-b border-slate-700 py-3 lg:py-4 gap-3 text-lg">
+    const isEspense = transaction.category.type == 'EXPENSE'
+    const handleDbClick = async (e) => {
+        const confirmed = window.confirm('apakah kamu mau hapus transaksi ini ?')
+        if(!confirmed){
+            return ;
+        }
+        try {
+            await transactionApi.delete(transaction.id);
+            onDelete()
+        }catch(e){
+            alert('gagal hapus transaksi')
+        }
+    }
+
+    return <>  
+
+    {/* anah dinsi kita kan tampilin date (kalo boleh) */}
+    {
+        showDate && <div>{formatDate(transaction.date)}</div>
+    }
+
+    <div onDoubleClick={handleDbClick} className="cursor-pointer flex lg:items-center justify-between border-b border-slate-700 py-3 lg:py-4 gap-3 text-lg">
   <div className="flex lg:items-center gap-3">
-    <div className="aspect-[1/1] h-[40px] flex items-center justify-center bg-red-200 border-2 border-red-300 rounded-full">
-      Icon
+    <div className={`aspect-[1/1] h-[40px] flex items-center justify-center border-2 ${isEspense ?'bg-red-200 border-red-300' : 'bg-green-200 border-green-300'} rounded-full`}>
+      {transaction.category.icon}
     </div>
     <div>
       <div className="flex flex-col lg:flex-row lg:items-center lg:gap-3">
         <div className="font-medium">
-          Category
+          {transaction.category?.name}
         </div>
         <div className="text-slate-400 text-sm lg:text-[1rem]">
-          Wallet name
+          {transaction.wallet?.name}
         </div>
       </div>
-      <div className="text-slate-400 text-xs lg:text-sm">Note (optional)</div>
+      <div className="text-slate-400 text-xs lg:text-sm">{transaction.note}</div>
     </div>
   </div>
   <div className="amount font-medium">
-    Rp 250.000
+    {formatCurrency(transaction.amount, transaction.wallet?.currency_code)}
   </div>
 </div>
 </>
