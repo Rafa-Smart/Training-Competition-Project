3d78c2d75df9031743a616f6df6837521bff8f25
Rafa_Khadafi <rafa.engineer.dev@gmail.com>
2026-05-05 09:32:35 +0700

    push

---
+++ CLIENT-COURSE/finance5/src/compoennets/TransactionItem.jsx
@@ -0,0 +1,44 @@
+import { transactionApi } from "../api/transaction";
+import { walletApi } from "../api/wallet";
+import { formatCurrency } from "../utils/format";
+
+export default TransactionItem = ({ transaction, showDate, onDelete }) => {
+  const handledblClick = async (e) => {
+    const confirmed = window.confirm("yakin delete transaksi ?");
+    if (!confirmed) return;
+    try {
+      await transactionApi.destroy(transaction.id);
+      onDelete();
+    } catch (e) {
+      alert("gagal delete");
+    }
+  };
+  return (
+    <>
+      <div class="cursor-pointer flex lg:items-center justify-between border-b border-slate-700 py-3 lg:py-4 gap-3 text-lg">
+        <div class="flex lg:items-center gap-3">
+          <div class="aspect-[1/1] h-[40px] flex items-center justify-center bg-red-200 border-2 border-red-300 rounded-full">
+            {transaction.category.icon}
+          </div>
+          <div>
+            <div class="flex flex-col lg:flex-row lg:items-center lg:gap-3">
+              <div class="font-medium">{transaction.category.name}</div>
+              <div class="text-slate-400 text-sm lg:text-[1rem]">
+                {transaction.wallet.name}
+              </div>
+            </div>
+            <div class="text-slate-400 text-xs lg:text-sm">
+              {transaction.note}
+            </div>
+          </div>
+        </div>
+        <div class="amount font-medium">
+          {formatCurrency(
+            transaction.amount,
+            transaction?.wallet?.currency_code,
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
