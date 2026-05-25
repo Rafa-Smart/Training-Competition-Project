1bca375a2fdf35e2fac09dfdaeeba8cafa2f84a7
Rafa_Khadafi <rafa.engineer.dev@gmail.com>
2026-05-25 20:29:51 +0700

    apaabar

--- MOBILE-COURSE/apakabar3/src/api/api.js
+++ MOBILE-COURSE/apakabar3/src/api/api.js
@@ -1,16 +1,16 @@
-const get = (url, params = {}) => {
-    const url = new URL(`http://localhost:8000/${url}`);
+const get = (urlnya, params = {}) => {
+    const url = new URL(`http://localhost:8000/${urlnya}`);
     Object.entries(params).forEach(([key, value]) => {
         if(key != undefined && value != undefined ) {
             url.searchParams.set(key, value)
         }
-        return fetch(url).then((data) => data.json())
     })
+    return fetch(url).then((data) => data.json())
 }
 
 
 export const api ={
-    getPosts:(params) => get('api/v1/posts'),
+    getPosts:(params) => get('api/v1/posts', params),
     getPost:(slug) => get('api/v1/posts/'+slug),
     getCategories:() => get('api/v1/categories')
 }
--- MOBILE-COURSE/apakabar3/src/pages/Settings.jsx
+++ MOBILE-COURSE/apakabar3/src/pages/Settings.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
+import { api } from "../api/api";
+const key = 'apakabar_categories'
 const Settings = ({theme, setTheme}) => {
+
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        api.getCategories().then(response => {
+            setCategories(response.data);
+        })
+    }, []) 
+
+    const toggleCategory = (category) => {
+        setCategories(prev => {
+            const next = prev.includes(category) ? prev.filter(c => c!=category):[...prev, category];
+            localStorage.setItem(key, JSON.stringify(prev));
+            return next
+        })
+    };
+
+
+
     return <div className='page'>
-        <h1>ini settings</h1>
+        <section ></section>
     </div>
 }
 
